4789d2f5747359732b60b5d9515c118f87089796
KanThor73 <you@example.com>
2023-06-08 17:26:23 +0200

    init du projet avec les classes main/perso/arme

---
+++ src/main/java/fr/discord/goodteam/Game.java
@@ -0,0 +1,25 @@
+package fr.discord.goodteam;
+
+public class Game {
+    public static void main(String[] args) {
+
+        Personnage perso1 = new Personnage("David", 500, 80);
+        Personnage perso2 = new Personnage("Goliath", 800, 40);
+
+        while (perso1.estVivant() && perso2.estVivant()) {
+            System.out.println(perso1.getNom() + "attaque" + perso2.getNom());
+            perso1.attaque(perso2);
+            perso2.afficherStatut();
+
+            System.out.println(perso2.getNom() + "attaque" + perso1.getNom());
+            perso2.attaque(perso1);
+            perso1.afficherStatut();
+        }
+
+        if (perso1.estVivant()) {
+            System.out.println(perso1.getNom() + "a vaincu" + perso2.getNom());
+        } else {
+            System.out.println(perso2.getNom() + "a vaincu" + perso1.getNom());
+        }
+    }
+}
