0c36969d225e60b50c2c81e841d4e7338151d89b
KanThor73 <you@example.com>
2023-06-08 17:59:16 +0200

    creation mage/guerrier

---
+++ src/main/java/fr/discord/goodteam/Guerrier.java
@@ -0,0 +1,9 @@
+package fr.discord.goodteam;
+
+public class Guerrier extends Personnage {
+
+    public Guerrier(String nom, int pv, int atk) {
+        super(nom, pv, atk);
+    }
+
+}
---
+++ src/main/java/fr/discord/goodteam/Mage.java
@@ -0,0 +1,9 @@
+package fr.discord.goodteam;
+
+public class Mage extends Personnage {
+
+    public Mage(String nom, int pv, int atk) {
+        super(nom, pv, atk);
+    }
+
+}
